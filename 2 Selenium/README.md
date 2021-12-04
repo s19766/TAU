1273cda9f386e2c68114379c007355c5b5bd0843
Damian Eggert <s19766@pjwstk.edu.pl>
2021-12-04 22:26:02 +0100

    Add exercise 2 - basic selenium tests for chrome and firefox

---
+++ 2 Selenium/README.md
@@ -0,0 +1,7 @@
+#SCENARIO
+1. Wejście na stronę morele.net
+2. Zaleznienie przycisku logowania
+3. Przejście do strony logowania
+4. Próba zalogowania się (login@wp.pl, password)
+5. Znaleznienie otrzymanego błedu logowania
+6. Potwierdzenie otrzymanego błedu logowania
---
+++ 2 Selenium/selenium_firefox.py
@@ -0,0 +1,50 @@
+# Damian Eggert s19766
+
+import os
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import logging
+
+
+logger = logging.getLogger('exmaple')
+logger.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+browser = webdriver.Firefox(service=Service(f"{os.path.abspath(os.getcwd())}/geckodriver"))
+
+logger.info('Go to morele.net')
+
+browser.get('https://www.morele.net/')
+
+logger.info('Click login/register button')
+
+browser.find_element(By.XPATH, "/html/body/div[2]/header/div/div/div/div[2]/div/div[2]/div/div[4]/a").click()
+
+time.sleep(1)
+
+logger.info('Try to login')
+
+browser.find_element(By.XPATH, '//*[@id="username"]').send_keys("Login@wp.pl")
+browser.find_element(By.XPATH, '//*[@id="password-log"]').send_keys("Password")
+browser.find_element(By.XPATH, "/html/body/main/div/div/div[3]/form/button").click()
+
+time.sleep(1)
+
+logger.info('Find login error')
+
+browser.find_element(By.XPATH, "/html/body/div[4]/div/div[2]/div[2]")
+
+time.sleep(2)
+
+logger.info('Confirm the received error')
+
+browser.find_element(By.XPATH, "/html/body/div[2]/div/div/button").click()
+
+browser.close()
